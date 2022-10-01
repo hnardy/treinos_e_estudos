9bc2881ba2da999a9094945c961d498928ba17bb
Henrique_Nardy <hnardyluz@gmail.com>
2022-10-01 00:58:28 -0300

    circulo pronto

--- circulo.py
+++ circulo.py
@@ -1,39 +1,15 @@
 import matplotlib.pyplot as plt
 import math
 
+x=[]
+y=[]
 
-a=[]
-b=[]
+raio = 1
+ang= 360
 
-na=[]
-nb=[]
-r=100
-for c in range(0,r):
-    a.append(c)
-    b.append( math.sqrt( (r-c)**2 + r**2)  )
+for c in range(0,ang):
+    x.append(math.cos(c) *raio)
+    y.append(math.sin(c) *raio)
 
-for c in a:
-    na.append(-1*c)
-for c in b:
-    nb.append(-1*c)
-
-
-
-
-
-
-
-plt.scatter(0,0)
-plt.scatter(a,b)
-plt.scatter(b,a)
-plt.scatter(na,b)
-plt.scatter(b,na)
-plt.scatter(nb,a)
-plt.scatter(a,nb)
-plt.scatter(na,a)
-plt.scatter(a,na)
-plt.scatter(b,nb)
-plt.scatter(nb,b)
-plt.scatter(na,nb)
-plt.scatter(nb,na)
+plt.scatter(x,y)
 plt.show()
