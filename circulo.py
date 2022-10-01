b5f3e573fb082ccbe5b00c4e1de513f77eb226bf
Henrique_Nardy <hnardyluz@gmail.com>
2022-10-01 00:31:37 -0300

    trapézio bonito

--- circulo.py
+++ circulo.py
@@ -1,24 +1,39 @@
 import matplotlib.pyplot as plt
 import math
-#iniciada os arrays
-x = []
-y = []
-raio = 5
-
-# a^2=b^2+c^2
-# a = raio logo
-#  raio = raiz quadrada de a^2 + b^2
-
-for c in range(1,10):
-    x.append(c)
-
-for q in x:
-    y.append(math.sqrt(
-    raio**2 +    q**2
-    ))
-
-print(x,y)
 
 
-plt.scatter(x,y)
+a=[]
+b=[]
+
+na=[]
+nb=[]
+r=10
+for c in range(0,r):
+    a.append(c)
+    b.append( math.sqrt( (r-c)**2 + r**2)  )
+
+for c in a:
+    na.append(-1*c)
+for c in b:
+    nb.append(-1*c)
+
+
+
+
+
+
+
+plt.scatter(0,0)
+plt.scatter(a,b)
+plt.scatter(b,a)
+plt.scatter(na,b)
+plt.scatter(b,na)
+plt.scatter(nb,a)
+plt.scatter(a,nb)
+plt.scatter(na,a)
+plt.scatter(a,na)
+plt.scatter(b,nb)
+plt.scatter(nb,b)
+plt.scatter(na,nb)
+plt.scatter(nb,na)
 plt.show()
